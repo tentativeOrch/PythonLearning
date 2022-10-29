05a6f2f582558d4b215a938dbfd029100480adf7
Michael Trujillo <mike119432@live.com>
2022-10-28 21:13:35 -0500

    finished chapter 1

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,6 @@
-print("hello world")
+print('Hello World')
+print('What is your name') #ask for name here
+name = input()
+print('It is good to meet you, ' + name)
+print('The length of your name is ')
+print(len(name))
